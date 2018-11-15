c572ebb3d9f3a3ee651956d90038572908b6a3e4
kanekona <amausakonata1932@gmail.com>
2018-11-15 20:31:10 +0900

    Comment 追記

--- OG2D/OG2D/src/Object/Effect/Effect.h
+++ OG2D/OG2D/src/Object/Effect/Effect.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "OGSystem\ResourceManager\ResourceManager.h"
 #include "OG\_OGsystem.h"
+#include "Object\GameObject.h"
+/**
+*Stateと同様に設計するが、
+*更新処理は描画処理はEngineSystemに登録して自動的に行わせる
+*そのため大元はGameObjectを使用する
+*当たり判定系をnullptrにすれば軽くなる？
+*/
 class EffectManager : private NonCopyable
 {
 	ResourceManager* rm;
